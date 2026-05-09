258e3e305f3eaa5bb14ca19a08450bb4d979e5d1
Hanna Geng <genghanna@gmail.com>
2026-05-10 04:33:06 +1000

    Create temperature_converter.py

---
+++ temperature_converter.py
@@ -0,0 +1,16 @@
+# # Temperature Converter
+
+choice = input("Convert (C)elsius or (F)ahrenheit? ")
+
+if choice.upper() == "C":
+    celsius = int(input("Enter Celsius: "))
+    fahrenheit = round(celsius * 9 / 5 + 32)
+    print("Fahrenheit:", fahrenheit)
+
+elif choice.upper() == "F":
+    fahrenheit = int(input("Enter Fahrenheit: "))
+    celsius = round((fahrenheit - 32) * 5 / 9)
+    print("Celsius:", celsius)
+
+else:
+    print("Invalid choice")
